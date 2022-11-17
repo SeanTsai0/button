3f4d1d38740cf9d98f2d1b203aa4bb34a84ec3b4
SeanTsai0 <SeanTsai0@gmail.com>
2022-11-17 14:28:04 +0800

    新增image/pic4

--- lib/main.dart
+++ lib/main.dart
@@ -34,31 +34,61 @@ class MyStatelessWidget extends StatelessWidget {
             style: ElevatedButton.styleFrom(
                 primary: Colors.yellow,
                 onPrimary: Colors.red,
-                textStyle: const TextStyle(fontSize: 20),
+                textStyle: const TextStyle(fontSize: 15),
                 shadowColor: Colors.black,
                 elevation: 10,
             ),
             onPressed: (){},
             child: Text('RaisedButton'),
           ),
-          const SizedBox(height: 30),
+          const SizedBox(height: 15),
           ElevatedButton(
             style: ElevatedButton.styleFrom(
               primary: Colors.blue,
-
-              textStyle: const TextStyle(fontSize: 20)
             ),
               onPressed: (){},
               child: Text('FlatButton')
           ),
-          const SizedBox(height: 30),
-          OutlinedButton(
-            style: OutlinedButton.styleFrom(
-              primary: Colors.black,
-              side: BorderSide(width: 2,color: Colors.red),
+          const SizedBox(height: 15),
+          ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
             ),
+            onPressed: (){final snackBar = SnackBar(
+              content: const Text('你按下RaisedButton'),
+              duration: Duration(seconds: 3),
+              backgroundColor: Colors.blue,
+              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+              action: SnackBarAction(
+                label: 'Toast訊息',
+                textColor: Colors.white,
+                onPressed: (){}
+              ),
+            );
+            ScaffoldMessenger.of(context).showSnackBar(snackBar);},
+            child: Text('你按下SnackBar')),
+          const SizedBox(height: 15),
+          IconButton(
+            icon: Icon(Icons.phone_android,color: Colors.blue),
             onPressed: (){},
-            child: Text('OutLineButton')
+            iconSize: 40,
+          ),
+          const SizedBox(height: 15),
+          FloatingActionButton(
+              onPressed: (){},
+            child: const Icon(Icons.phone_android),
+          ),
+          const SizedBox(height: 15),
+          ElevatedButton.icon(
+            onPressed: (){},
+            icon: Icon(Icons.phone_android),
+            label: Text('RaisedButton.icon'),
+            style: ElevatedButton.styleFrom(
+              shadowColor: Colors.black,
+              elevation: 10,
+              onPrimary: Colors.red,
+              primary: Colors.white70,
+            ),
           ),
         ],
       ),
